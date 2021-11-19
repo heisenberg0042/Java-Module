951c99847e40643501c319bcb1eb186f0badde0e
heisenberg0042 <94596495+heisenberg0042@users.noreply.github.com>
2021-11-19 19:45:33 +0530

    Add files via upload

---
+++ nov_19/DataTypes.java
@@ -0,0 +1,53 @@
+package nov_19;
+
+class DataTypes {
+
+    // properties
+
+    // methods
+
+    // data types
+
+    // primitive // non-primitive datatype
+
+    // 8 different primitive types
+
+    // byte   // 1 byte   // 8 bit  / 10101010
+
+    // -127 // 128
+
+    byte b = 127 ;   // memory identifier
+
+    // short  // 2 byte  // 16 bit  // 10101010101
+
+    // -32768 // 32767
+
+    short s = 32767;
+
+    // int // 4 byte // 32 bit
+
+    int i = 1234567890;
+
+    // long  // 8  byte
+
+    //float  // 4 byte  //
+
+    // double // 8 byte
+
+    float f = 0.1f;
+
+    // char   // 1 byte
+
+    char ch = 'g';
+
+    // boolean  // 1 byte
+
+    boolean bol = false;
+
+    // non primitive datatype
+
+//    String name = "tops";
+
+
+
+}
---
+++ nov_19/HelloWorld.java
@@ -0,0 +1,21 @@
+package nov_19;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        int i = 234343;
+
+        float f1 = 3434.3434f;
+
+        System.out.println(i);
+
+        System.out.println("Value of Float is : "+f1);   // concat
+
+        System.out.println("Value of Float is : "+f1+" this "+i+" kem cho");
+
+
+    }
+}
